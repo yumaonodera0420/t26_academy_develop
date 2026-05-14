c541f021aaa343ef9eb386c1306d7733826c1f23
MT 小野寺 勇磨 <yuma.onodera@metateam.co.jp>
2026-05-14 21:35:50 +0900

    書籍管理機能修正

--- src/main/java/jp/co/metateam/library/service/BookMstService.java
+++ src/main/java/jp/co/metateam/library/service/BookMstService.java
@@ -1,34 +1,34 @@
 package jp.co.metateam.library.service;
- 
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
- 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
- 
+
 import io.micrometer.common.util.StringUtils;
 import jp.co.metateam.library.model.BookMst;
 import jp.co.metateam.library.model.BookMstDto;
 import jp.co.metateam.library.repository.BookMstRepository;
- 
+
 @Service
 public class BookMstService {
- 
+
     private final BookMstRepository bookMstRepository;
- 
+
     @Autowired
     public BookMstService(BookMstRepository bookMstRepository) {
         this.bookMstRepository = bookMstRepository;
     }
- 
+
     public List<BookMstDto> findAvailableWithStockCount() {
         List<BookMst> books = this.bookMstRepository.findLimitedBook();
         List<BookMstDto> bookMstDtoList = new ArrayList<BookMstDto>();
- 
+
         // 書籍の在庫数を取得
         // FIXME: 現状は書籍ID毎にDBに問い合わせている。一度のSQLで完了させたい。
         for (int i = 0; i < books.size(); i++) {
@@ -39,10 +39,10 @@ public class BookMstService {
             bookMstDto.setTitle(book.getTitle());
             bookMstDtoList.add(bookMstDto);
         }
- 
+
         return bookMstDtoList;
     }
- 
+
     @Transactional
     public void save(BookMstDto bookMstDto) {
         BookMst entity = new BookMst(); // BookMstDtoからBookMstへの変換
@@ -51,10 +51,9 @@ public class BookMstService {
         // データベースへの保存
         bookMstRepository.save(entity);
     }
- 
+
     public Optional<BookMst> findByIsbn(String isbn) {
         return bookMstRepository.findByIsbn(isbn);
     }
- 
+
 }
- 
