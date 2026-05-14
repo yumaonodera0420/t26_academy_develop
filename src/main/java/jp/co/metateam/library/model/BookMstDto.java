e33b6b27069970b70372e60c1d69a8ff102bba6f
MT 小野寺 勇磨 <yuma.onodera@metateam.co.jp>
2026-05-14 21:44:25 +0900

    書籍管理機能修正

--- src/main/java/jp/co/metateam/library/model/BookMstDto.java
+++ src/main/java/jp/co/metateam/library/model/BookMstDto.java
@@ -7,28 +7,28 @@ import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
-
+ 
 /**
  * 書籍マスタDTO
  */
 @Getter
 @Setter
 public class BookMstDto {
-
+ 
     private Long id;
-
+ 
     // ISBN
     @NotEmpty(message = "ISBNは必須です")
     @Size(min = 13, max = 13, message = "ISBNは13桁で入力してください")
     @Pattern(regexp = "^$|^[0-9]+$", message = "ISBNは半角数字のみで入力してください")
     private String isbn;
-
+ 
     // 書籍名
     @NotEmpty(message = "書籍名は必須です")
     @Size(max = 255, message = "書籍名は255文字以下で入力してください")
     private String title;
-    
+
     private Timestamp deletedAt;
-    
+
     private BookMst bookMst;
 }
--- src/main/java/jp/co/metateam/library/repository/BookMstRepository.java
+++ src/main/java/jp/co/metateam/library/repository/BookMstRepository.java
@@ -1,8 +1,8 @@
 package jp.co.metateam.library.repository;
-
+ 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-
+ 
 import jp.co.metateam.library.model.BookMst;
 import java.util.List;
 import java.util.Optional;
